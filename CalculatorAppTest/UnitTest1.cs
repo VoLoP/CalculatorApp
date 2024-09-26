b17f574fe571fb6ce0918a940792a6e8ca3f735a
Simone <bativolpet1@gmail.com>
2024-09-26 11:21:44 +0200

    Implemented Test Class

--- CalculatorAppTest/UnitTest1.cs
+++ CalculatorAppTest/UnitTest1.cs
@@ -3,14 +3,18 @@ namespace CalculatorAppTest
     public class Tests
     {
         [SetUp]
+
+        CalculatorClass myCalc;
         public void Setup()
         {
+            myCalc = new CalculatorClass();
         }
 
         [Test]
-        public void Test1()
+        public void EnterTwoValidNumbersReturnsAValidNumber()
         {
-            Assert.Pass();
+            var res = myCalc.Add(2, 3);
+            Assert.That(res, Is.EqualTo(5), "3+2 must returns 5");
         }
     }
 }
